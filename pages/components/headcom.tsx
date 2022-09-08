7c446f3aa61b79dc11ab7d04ee7d49a86e06208a
yuukiiwai <yuuki.20020217@gmail.com>
2022-09-08 22:22:56 +0900

    add google ad script

--- pages/components/headcom.tsx
+++ pages/components/headcom.tsx
@@ -4,7 +4,7 @@ const Headcom:FC = () => {
     return(
         <>
             <link rel="icon" href="/favicon.ico" />
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8707396742877439" crossOrigin="anonymous"></script>
+            <script async src={"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client="+process.env.NEXT_PUBLIC_GADCLICODE} crossOrigin="anonymous"></script>
         </>
     );
 }
